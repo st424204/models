73910ec3471763d0ea8dd3f4beee0ea267ef3f3d
Billy <st424204@yahoo.com.tw>
2017-07-12 12:49:45 +0800

    'Initial'

--- fine_tune.py
+++ fine_tune.py
@@ -1,29 +1,33 @@
-from keras.applications.vgg16 import VGG16
+from keras.applications.inception_v3 import InceptionV3
 from keras.preprocessing import image
 from keras.models import Model,load_model
 from keras.layers import Dense,GlobalAveragePooling2D,Dropout
+from keras.callbacks import ModelCheckpoint
 from keras import backend as K
 import pandas as pd
 import numpy as np
 from itertools import chain
 
-def mygen(batch_size):
+def mygen(batch_size,valid):
 	df = pd.read_csv('train_v2.csv')
 	labels = sorted(set(chain.from_iterable([tag.split() for tag in df['tags'].values])))
 	y_map =  {tag: index for index, tag in enumerate(labels)}
-	x_train = np.zeros([batch_size,64,64,3])
+	x_train = np.zeros([batch_size,256,256,3])
 	y_train = np.zeros([batch_size,len(y_map)])
+	index = 0
+	if valid:
+		df = df[len(df)*8//10:]
+	else:
+		df = df[:len(df)*8//10]
 	while True:
-		index = 0
-		for x in df.sample(batch_size).itertuples():
-			x_train[index]=image.img_to_array(image.load_img('train-jpg/%s.jpg'%(x[1]),target_size=(64, 64)))
-			for tag in x[2].split():
-				y_train[index][y_map[tag]]=1
-			index +=1
+		x_train[index%batch_size]=image.img_to_array(image.load_img('train-jpg/%s.jpg'%(df['image_name'][index%len(df)]),target_size=(256,256)))
+		for tag in df['tags'][index%len(df)].split():
+			y_train[index%batch_size][y_map[tag]]=1
+		index +=1
 		yield x_train,y_train
 			
 			
-base_model = VGG16(weights='imagenet',include_top=False,input_shape=(64,64,3))
+base_model = InceptionV3(weights='imagenet',include_top=False,input_shape=(256,256,3))
 x = base_model.output
 x = GlobalAveragePooling2D()(x)
 x = Dense(1024,activation='relu')(x)
@@ -36,9 +40,11 @@ for layer in base_model.layers:
 model.compile(optimizer='rmsprop',loss='binary_crossentropy',metrics=['acc'])
 
 model.fit_generator(
-		generator=mygen(128),
-		steps_per_epoch=2000,
-		epochs=10,
+		generator=mygen(10,False),
+		steps_per_epoch=3000,
+		epochs=2,
+		validation_data=mygen(10,True),
+		validation_steps = 800,
 		verbose=1,
+		callbacks=[ModelCheckpoint('model.h5', monitor='val_acc', verbose=1, save_best_only=True),],
 	)
-model.save('model.h5')
